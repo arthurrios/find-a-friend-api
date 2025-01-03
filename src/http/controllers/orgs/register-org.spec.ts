5e2da0e9fd5ca0a0351ac061f7f57ab874b3e523
arthur <arthur.rios007@gmail.com>
2025-01-03 20:38:50 -0300

    feat: add register org test e2e

--- src/http/controllers/orgs/register-org.spec.ts
+++ src/http/controllers/orgs/register-org.spec.ts
@@ -1,3 +1,31 @@
-import { test } from 'vitest'
+import { app } from '@/app'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('ok', () => {})
+describe('Register Org (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+  it('should register a new org', async () => {
+    const response = await request(app.server).post('/orgs').send({
+      name: 'Org name',
+      owner_name: 'Owner name',
+      email: 'org@email.com',
+      whatsapp: '61999999999',
+      password: '123456',
+      cep: '70000000',
+      state: 'DF',
+      city: 'Brasília',
+      neighborhood: 'Plano Piloto',
+      street: 'Quadra 1',
+      latitude: 0,
+      longitude: 0,
+    })
+
+    expect(response.status).toBe(201)
+  })
+})
